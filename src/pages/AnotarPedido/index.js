09fc4a573f1611d76ebb2e07d5694e90854cc93d
LauraDeperon <lauradeperonribeiro@gmail.com>
2021-02-24 14:58:56 -0300

    ajustes eslint

--- src/pages/AnotarPedido/index.js
+++ src/pages/AnotarPedido/index.js
@@ -11,9 +11,13 @@ function AnotarPedidos() {
       <HeaderMain />
       <section className="container-description">
         <img src={IconOrder} alt="icon-order" className="icon-order"></img>
-        <h2><Link to='/AnotarPedidos'>Anotar Pedidos</Link></h2>
+        <h2>
+          <Link to="/AnotarPedidos">Anotar Pedidos</Link>
+        </h2>
         <img src={IconOrder} alt="icon-order" className="icon-order"></img>
-        <h2><Link to='/PedidosProntos'>Pedidos Prontos</Link></h2>
+        <h2>
+          <Link to="/PedidosProntos">Pedidos Prontos</Link>
+        </h2>
       </section>
       <CardapioCafeManha />
     </div>
