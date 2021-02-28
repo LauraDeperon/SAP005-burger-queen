4c5013ffb5effa9018280478efe952724dde7e90
alencartha <alencar_tha@hotmail.com>
2021-02-27 22:40:24 -0300

    Adicionado resumo de pedido na area superior da página, estilo do menu com imagem dos itens e inicio de trabalho com modal para informar possiveis erros ao fazer login

---
+++ src/components/Modals/ModalLogin/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../../../Styles/ModalLogin.css';
+
+function ModalLogin(message) {
+  const mensagem = message
+
+  return (
+
+    <section className="modalDialog">
+
+      <div>
+        <a title="Fechar"></a>
+      </div>  
+
+      <div>
+        <p>Preencha todos os campos corretamente {mensagem}</p>     
+      </div>      
+
+    </section>
+  )
+};
+
+export default ModalLogin;
