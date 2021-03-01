bcdce023b46f98a8ab8262cd56a09abacb1af45f
LauraDeperon <lauradeperonribeiro@gmail.com>
2021-03-01 17:12:33 -0300

    formatacao do codigo eslint

--- src/components/Modals/ModalLogin/index.js
+++ src/components/Modals/ModalLogin/index.js
@@ -2,22 +2,19 @@ import React from 'react';
 import '../../../Styles/ModalLogin.css';
 
 function ModalLogin(message) {
-  const mensagem = message
+  const mensagem = message;
 
   return (
-
-    <section className='modalDialog'>
+    <section className="modalDialog">
+      <div>
+        <a title="Fechar"></a>
+      </div>
 
       <div>
-        <a title='Fechar'></a>
-      </div>  
-
-      <div>
-        <p>Preencha todos os campos corretamente {mensagem}</p>     
-      </div>      
-
+        <p>Preencha todos os campos corretamente {mensagem}</p>
+      </div>
     </section>
-  )
-};
+  );
+}
 
 export default ModalLogin;
